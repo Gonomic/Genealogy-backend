0a8e89dfd993ffd8f5c029b74956de2378e338e2
Frans Dekkers <fde.gma@gmail.com>
2020-02-01 00:27:05 +0100

    Progressed on Children screen

---
+++ AddChildToParent.sql
@@ -0,0 +1,50 @@
+CREATE DEFINER=`root`@`%` PROCEDURE `AddChildToParent`(IN Parent INT, IN Child INT)
+BEGIN
+
+	-- ----------------------------------------------------------------------------------------------------------------------------------------------
+    -- Author: 	Frans Dekkers (GoNomics)
+    -- Date:	31-01-2020
+    -- -----------------------------------
+    -- Prurpose of this Sproc:
+    -- Add a child to a parent
+    -- 
+    -- Parameters of this Sproc:
+    -- 'Parent'= The person to add the child to
+    -- 'Child'= The person to add as child
+    -- 
+    -- High level flow of this Sproc:
+    -- => Simply add a record to table relations which ties one person as a child to another person as a father
+    --   
+    -- Note:	None
+    --		
+    -- TODO's:
+    -- => 01/02/2020 -> Determine if the parent is a father or mother and then add the correct relation
+    -- => 01/02/2020 -> Add logic for if the transaction fails (take one of the other Sprocs as example)
+    -- ----------------------------------------------------------------------------------------------------------------------------------------------
+    
+    DECLARE CompletedOk INT;
+    DECLARE NewTransNo INT;
+    DECLARE TransResult INT;
+	
+    SET CompletedOk = true;
+    SET TransResult = 0;
+    SET NewTransNo = GetTranNo("AddChildToParent");
+    
+      
+    INSERT INTO humans.testlog 
+		SET TestLog = CONCAT('TransAction-', IFNULL(NewTransNo, 'null'), '. TransResult= ', IFNULL(TransResult, ''),
+							 '. Start SPROC: AddChildToParent(). Add a child to a parent. Parent= ', IFNULL(Parent, 'null'), '. Child= ', IFNULL(Child, 'null')),
+			TestLogDateTime = NOW();
+    
+	INSERT INTO relations (RelationName, RelationPerson, RelationWithPerson) 
+	VALUES (1, Parent, Child );
+   
+	INSERT INTO humans.testlog 
+		SET TestLog = CONCAT('TransAction-', IFNULL(NewTransNo, 'null'), 
+			'. TransResult= ', IFNULL(TransResult, ''),
+			'. End SPROC: AddChildToParent(). Added child: ', IFNULL(Child, 'null'), 'to parent: ', IFNULL(Parent, 'null')),
+			TestLogDateTime = NOW();
+
+SELECT 'OK';
+   
+END

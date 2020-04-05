d4683bebca3e458e60a80e67b79ac88fcb4804dd
Frans Dekkers <fde.gma@gmail.com>
2020-04-05 16:26:22 +0200

    Some file management related changes

---
+++ writeinitgenbedata.bat
@@ -0,0 +1,1 @@
+docker run --rm --volumes-from GENBE01 -v d:/Frans/Development/Gen/Genealogy-backend/xfergenbedata:/xfergenbedata ubuntu bash -c "cd /var/lib/mysql && tar xvf /xfergenbedata/initgenbedata.tar"

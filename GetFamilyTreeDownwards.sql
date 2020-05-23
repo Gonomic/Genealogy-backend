4fca51dc6fb061b206f0afea9b589e5449bcdba5
Frans Dekkers <fde.gma@gmail.com>
2020-05-23 09:33:33 +0200

    Further progress on using MySQL CTE  as basis for drawing famtree.

--- GetFamilyTreeDownwards.sql
+++ GetFamilyTreeDownwards.sql
@@ -6,6 +6,8 @@ BEGIN
     DECLARE MessageText CHAR;
 	DECLARE ReturnedSqlState INT;
 	DECLARE MySQLErrNo INT;
+    DECLARE UniqueID BINARY(16);
+    DECLARE UniqueIdString VARCHAR(36);
         
 	DECLARE EXIT HANDLER FOR SQLEXCEPTION
 		BEGIN
@@ -27,6 +29,8 @@ BEGIN
 main_proc:
 	BEGIN
 	START TRANSACTION;
+    
+
 
 	SET CompletedOk = 0;
 
@@ -34,16 +38,29 @@ main_proc:
 
 	SET NewTransNo = GetTranNo("GetFamilyTreeDownwards");
     
-    IF logIn THEN
+	IF logIn THEN
 		INSERT INTO humans.testlog 
 		SET TestLog = CONCAT('TransAction-', IFNULL(NewTransNo, 'null'), '. Start GetFamilyTreeDownwards() with params: personIdIn= ', IFNULL(personIdIn, "null"), ' , numberOfGenerations= ', IFNULL(numberOfGenerationsIn, "null")),
 			TestLogDateTime = NOW();
 	END IF;
+    
+    SET UniqueID = UNHEX(REPLACE(UUID(), '-', ''));
+    SELECT "UniqueID= ", UniqueID;
+    SET UniqueIdString = BIN_TO_UUID(UniqueID);
+    SELECT "UniqueIdString=", UniqueIdString;
+    
+	IF logIn THEN
+		INSERT INTO humans.testlog 
+		SET TestLog = CONCAT('TransAction-', IFNULL(NewTransNo, 'null'), '. GetFamilyTreeDownwards(): running CTE and copying into helpthem table for data persistance, UUID= ', IFNULL(UniqueIdString, 'null')),
+			TestLogDateTime = NOW();
+	END IF;
 
-	INSERT INTO helpthem (Limitter, PersonId, PersonName, PersonDateOfBirth, PersonDateOfDeath, PersonIsMale, Father, Mother, Partner, ParentsAreAlsoPartners) 
-    WITH RECURSIVE FamTree (Limitter, PersonId, PersonName, PersonBirth, PersonDeath, PersonIsMale, Father, Mother, Partner, ParentsArePartners) AS 
-    (
-        SELECT	1 as Limitter, 
+	INSERT INTO helpthem (UniqueId, Limitter, PersonId, PersonName, PersonDateOfBirth, PersonDateOfDeath, PersonIsMale, Father, Mother, Partner, ParentsAreAlsoPartners) 
+    (WITH RECURSIVE FamTree (UniqueID, Limitter, PersonId, PersonName, PersonBirth, PersonDeath, PersonIsMale, Father, Mother, Partner, ParentsArePartners) AS 
+		(
+			SELECT	
+				UniqueID,
+				1 as Limitter, 
 				PersonID, 
                 CONCAT(PersonGivvenName, ' ', PersonFamilyName) as PersonName,
 				PersonDateOfBirth, 
@@ -54,12 +71,14 @@ main_proc:
                 rp.RelationWithPerson as Partner,
                 fCheckIfParentsAreAlsoPartners(PersonID) as ParentsAreAlsoPartners
 			FROM persons p 
-            LEFT JOIN relations rf ON p.PersonID = rf.RelationPerson AND rf.RelationName = "1"
-            LEFT JOIN relations rm ON p.PersonID = rm.RelationPerson AND rm.RelationName = "2"            
-            LEFT JOIN relations rp ON p.PersonID = rp.RelationPerson AND rp.RelationName = "3"      
-            WHERE p.PersonId = personIdIn
-	    UNION ALL
-        SELECT	Limitter + 1,
+				LEFT JOIN relations rf ON p.PersonID = rf.RelationPerson AND rf.RelationName = "1"
+				LEFT JOIN relations rm ON p.PersonID = rm.RelationPerson AND rm.RelationName = "2"            
+				LEFT JOIN relations rp ON p.PersonID = rp.RelationPerson AND rp.RelationName = "3"      
+			WHERE p.PersonId = personIdIn
+			UNION ALL
+			SELECT	
+				UniqueID,
+                Limitter + 1,
 				p.PersonID, 
                 CONCAT(p.PersonGivvenName, ' ', p.PersonFamilyName) as PersonName,
 				p.PersonDateOfBirth, 
@@ -70,15 +89,30 @@ main_proc:
                 rp.RelationWithPerson as Partner,
                 fCheckIfParentsAreAlsoPartners(p.PersonID) as ParentsAreAlsoPartners
  			FROM persons p 
-            LEFT JOIN relations rf ON p.PersonID = rf.RelationPerson AND rf.RelationName = "1"
-            LEFT JOIN relations rm ON p.PersonID = rm.RelationPerson AND rm.RelationName = "2"            
-            LEFT JOIN relations rp ON p.PersonID = rp.RelationPerson AND rp.RelationName = "3"      
-            INNER JOIN FamTree FT ON FT.PersonId = rf.RelationWithPerson 
-			WHERE Limitter < numberOfGenerationsIn
-	) 
-    SELECT * FROM FamTree;
+				LEFT JOIN relations rf ON p.PersonID = rf.RelationPerson AND rf.RelationName = "1"
+				LEFT JOIN relations rm ON p.PersonID = rm.RelationPerson AND rm.RelationName = "2"            
+				LEFT JOIN relations rp ON p.PersonID = rp.RelationPerson AND rp.RelationName = "3"      
+				INNER JOIN FamTree FT ON FT.PersonId = rf.RelationWithPerson 
+				WHERE Limitter < numberOfGenerationsIn
+			) 
+			SELECT * FROM FamTree
+    );
     
-    SELECT * FROM helpthem ORDER by PersonDateOfBirth;
+	IF logIn THEN
+		INSERT INTO humans.testlog 
+		SET TestLog = CONCAT('TransAction-', IFNULL(NewTransNo, 'null'), '. GetFamilyTreeDownwards(): selecting all records created in this session, UUID= ', IFNULL(UniqueIdString, 'null')),
+			TestLogDateTime = NOW();
+	END IF;
+    
+    SELECT Limitter as Generation, PersonId, PersonName, PersonDateOfBirth, PersonDateOfDeath, PersonIsMale, Father, Mother, Partner, ParentsAreAlsoPartners FROM helpthem WHERE UniqueId = UniqueID ORDER by PersonDateOfBirth;
+    
+	IF logIn THEN
+		INSERT INTO humans.testlog 
+		SET TestLog = CONCAT('TransAction-', IFNULL(NewTransNo, 'null'), '. GetFamilyTreeDownwards(): deleting all records after select, created in this session, UUID= ', IFNULL(UniqueIdString, 'null')),
+			TestLogDateTime = NOW();
+	END IF;
+    
+    DELETE FROM helpthem WHERE UniqueId = UniqueID AND PrimKey > 0;
     
 	IF logIn THEN
 		INSERT INTO humans.testlog 

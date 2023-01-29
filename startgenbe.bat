fec58b4954e17513100d3067a24d52b699464022
Frans Dekkers <12582936+Gonomic@users.noreply.github.com>
2023-01-29 20:05:26 +0100

    Adapted Docker statements, now seem to work

--- startgenbe.bat
+++ startgenbe.bat
@@ -1,1 +1,1 @@
-docker run --name DEKKNET.COM -it -d -p 3306:3306 -v GENDATA01:/var/lib/mysql fransdekkers/genbackend:1.2
+docker run --name=FD-GEN-DB-1 -it -d -p=3306:3306 -v=MariaDB:/var/lib/mysql -e MARIADB_ROOT_PASSWORD=ErgGeheim --network=DN-GEN mariadb:latest
